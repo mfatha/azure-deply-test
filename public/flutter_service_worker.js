a47f82465d52c9411defbaed1e4e142ff2dcd78d
Mohammed-simbiotik <mfatha.fire@gmail.com>
2022-07-31 01:21:19 +0530

    1) login enter to submit 2) dashboard chart alignment 3) form builder sparate screen 4) view vendor change hover mouse pointer 5) organization show title vendor and clinet details 6) user credential show username 7) user credential creation issue

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "favicon.ico": "4694e582a65f2ff630556a77b34efd81",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "462ea2d1c53197386d29fb3472fc0021",
-"index.html": "f15092fec309b229a03f55018c86d114",
-"/": "f15092fec309b229a03f55018c86d114",
-"main.dart.js": "4a84a524d209f391ba4fc98362bdeb56",
+"index.html": "576af884009c6f2b62bad035692382df",
+"/": "576af884009c6f2b62bad035692382df",
+"main.dart.js": "d750ec79cb497ef2c65b26007f2e5532",
 "manifest.json": "8b975cc7aeb0a18161321ebe921d5f35",
 "version.json": "b665b454975d8dbc3248260a14141c76"
 };
