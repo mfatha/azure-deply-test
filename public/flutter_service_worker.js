b540234168bdbf13b797bb75d05e7382992efe7f
Mohammed-simbiotik <mfatha.fire@gmail.com>
2022-08-01 09:47:03 +0530

    logout changes

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "favicon.ico": "4694e582a65f2ff630556a77b34efd81",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "462ea2d1c53197386d29fb3472fc0021",
-"index.html": "b63e9729d2011d4e98a51380d2cebbb5",
-"/": "b63e9729d2011d4e98a51380d2cebbb5",
-"main.dart.js": "5d9ddbfdc9db4a16ae631fe21457de3a",
+"index.html": "c877856c82134a0d3df7b4388036e389",
+"/": "c877856c82134a0d3df7b4388036e389",
+"main.dart.js": "06788d57b0139c508f09c018af80900f",
 "manifest.json": "8b975cc7aeb0a18161321ebe921d5f35",
 "version.json": "b665b454975d8dbc3248260a14141c76"
 };
