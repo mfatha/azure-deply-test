772cb17777ba1d611df5b622a0252e3d77801519
Mohammed-simbiotik <mfatha.fire@gmail.com>
2022-08-03 19:56:13 +0530

    to stateful

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -48,9 +48,9 @@ const RESOURCES = {
 "favicon.ico": "4694e582a65f2ff630556a77b34efd81",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "462ea2d1c53197386d29fb3472fc0021",
-"index.html": "6743644840376be8b61499d8fd51aa60",
-"/": "6743644840376be8b61499d8fd51aa60",
-"main.dart.js": "88735eeb795fecd976c2bc657e59473b",
+"index.html": "2baf4d88da536b5929ca6961d8b454da",
+"/": "2baf4d88da536b5929ca6961d8b454da",
+"main.dart.js": "efe2e15386ffb2be19b5198240595167",
 "manifest.json": "8b975cc7aeb0a18161321ebe921d5f35",
 "version.json": "b665b454975d8dbc3248260a14141c76"
 };
