fa40f846f723d8fed703c9713f94bec691678f29
Mohammed Fathauddin <mfatha.fire@gmail.com>
2022-08-22 11:28:08 +0530

    file upload changes

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var multer  = require('multer')
+var fs = require('fs');
+var upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(logger('dev'));
@@ -13,3 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 module.exports = app;
+
+
+
+app.post('/upload', upload.single("picture"), function (req,res) {
+	console.log("Received file" + req.file.originalname);
+	var src = fs.createReadStream(req.file.path);
+	var dest = fs.createWriteStream('uploads/' + req.file.originalname);
+	src.pipe(dest);
+	src.on('end', function() {
+		fs.unlinkSync(req.file.path);
+		res.json('OK: received ' + req.file.originalname);
+	});
+	src.on('error', function(err) { res.json('Something went wrong!'); });
+
+});
