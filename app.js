c4588b3c60264567d80dd1b1a6c939f5fd33ee2e
Mohammed Fathauddin <mfatha.fire@gmail.com>
2022-08-22 16:41:20 +0530

    file upload to app server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var multer  = require('multer')
 var fs = require('fs');
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: 'public/uploads/' })
 
 var app = express();
 
@@ -21,14 +21,14 @@ module.exports = app;
 
 
 app.post('/upload', upload.single("picture"), function (req,res) {
-	console.log("Received file" + req.file.originalname);
-	var src = fs.createReadStream(req.file.path);
-	var dest = fs.createWriteStream('uploads/' + req.file.originalname);
-	src.pipe(dest);
-	src.on('end', function() {
-		fs.unlinkSync(req.file.path);
-		res.json('OK: received ' + req.file.originalname);
-	});
-	src.on('error', function(err) { res.json('Something went wrong!'); });
-
-});
+    console.log("Received file" + req.file.originalname);
+    var src = fs.createReadStream(req.file.path);
+    var dest = fs.createWriteStream('public/uploads/' + req.file.originalname);
+    src.pipe(dest);
+    src.on('end', function() {
+    	fs.unlinkSync(req.file.path);
+    	res.json('OK: received ' + req.file.originalname);
+    });
+    src.on('error', function(err) { res.json('Something went wrong!'); });
+  
+  });
