6a4f21b5c4f84df05230a2fbf9860187d947c50f
Mohammed Fathauddin <mfatha.fire@gmail.com>
2022-08-31 22:23:49 +0530

    minor image fix

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -51,9 +51,9 @@ const RESOURCES = {
 "favicon.ico": "4694e582a65f2ff630556a77b34efd81",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "462ea2d1c53197386d29fb3472fc0021",
-"index.html": "fb4ca0cdd8a03e862e264528ca7a4eff",
-"/": "fb4ca0cdd8a03e862e264528ca7a4eff",
-"main.dart.js": "39e5842a25c35cee73006fc51d6316cc",
+"index.html": "2b8689d7e974d506cca8a15768eb55eb",
+"/": "2b8689d7e974d506cca8a15768eb55eb",
+"main.dart.js": "4994e6b2befff09f84db377fb6d5b959",
 "manifest.json": "8b975cc7aeb0a18161321ebe921d5f35",
 "version.json": "b665b454975d8dbc3248260a14141c76"
 };
