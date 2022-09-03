ed1b5ea1b099ff1e6b4438d14df9275d8226859a
Mohammed Fathauddin <mfatha.fire@gmail.com>
2022-09-03 23:14:33 +0530

    bug fixes

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {
 "assets/assets/images/logo2-bbg.png": "e322f758229f8ad18a9334958c4eaf19",
 "assets/FontManifest.json": "396d292d777dede3077ac80d9e3c1fab",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "c77c8cc1f7d0d8d37f450e728bc08e6b",
+"assets/NOTICES": "c21c080f333bba87ccc822f4a625ab8c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -51,9 +51,9 @@ const RESOURCES = {
 "favicon.ico": "4694e582a65f2ff630556a77b34efd81",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "462ea2d1c53197386d29fb3472fc0021",
-"index.html": "2b8689d7e974d506cca8a15768eb55eb",
-"/": "2b8689d7e974d506cca8a15768eb55eb",
-"main.dart.js": "4994e6b2befff09f84db377fb6d5b959",
+"index.html": "a02bf85cd456b10bcd0d9384cc3c8b78",
+"/": "a02bf85cd456b10bcd0d9384cc3c8b78",
+"main.dart.js": "9642e6eea333758fdf7d607d63933a87",
 "manifest.json": "8b975cc7aeb0a18161321ebe921d5f35",
 "version.json": "b665b454975d8dbc3248260a14141c76"
 };
